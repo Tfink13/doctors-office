79744626a001cc3d0f11245e4a18878dc9b66b80
Tyler Fink <tsfink182@stevenscollege.edu>
2019-11-04 09:25:32 -0500

    Front end

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="login" method="post">
+      
+
+    </form>
+  </body>
+</html>
