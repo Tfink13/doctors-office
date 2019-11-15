ef6c4d9d66ed19d410871a181a5a62afcfaf2638
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-11-15 11:19:55 -0500

    fix pages affected by folder changes

--- logout.php
+++ logout.php
@@ -1,25 +1,8 @@
 <?php
 if (isset(($_POST['logout']))) {
   session_destroy();
-  header("Location: index.php");
+  session_unset();
+  header("Location: http://localhost/doctors-office/");
 
 }
-
- ?>
-
-
- <!DOCTYPE html>
- <html lang="" dir="ltr">
-   <head>
-     <link rel="stylesheet" href="master.css">
-     <meta charset="utf-8">
-     <title></title>
-   </head>
-   <body>
-
-     <form class="" action="index.php" method="post">
-       <button type="submit" name="logout">Log Out</button>
-     </form>
-
-   </body>
- </html>
+?>
